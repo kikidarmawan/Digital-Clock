2d05e14115b56020486da4f50506791d5cfe58a5
AlbyDKC1010 <albytita10@gmail.com>
2023-04-01 17:38:22 +0700

    update:ligh mode

--- pertemuan4/clock.js
+++ pertemuan4/clock.js
@@ -4,5 +4,13 @@ setInterval(() => {
     let jam = date.getHours();
     let menit = date.getMinutes();
     let detik = date.getSeconds();
+    if (detik < 10) {
+        detik = '0' + detik;
+    }
     clock.textContent = `${jam}:${menit}:${detik}`;
 }, 1000);
+
+const wrapper = document.querySelector('.wrapper');
+wrapper.addEventListener('click', function () {
+    wrapper.classList.toggle('.light-mode')
+})
